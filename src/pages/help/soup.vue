16f5fb1af1c3a32ceb39100be0a5bbf9a2a9a820
Jason Cheng <jasoncheng224@gmail.com>
2020-08-02 12:54:12 -0700

    testing soup request from frontend

--- src/pages/help/soup.vue
+++ src/pages/help/soup.vue
@@ -1,5 +1,109 @@
 <template>
-    <div>
-        Find soup kitchens - TODO
-    </div>
+  <div style="height: 500px; width: 100%">
+    <button @click="showLongText">Toggle long popup</button>
+    <button @click="showMap = !showMap">Toggle map</button>
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker :lat-lng="withPopup">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div>
 </template>
+
+<script>
+
+import "leaflet/dist/leaflet.css";
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+import axios from '@/plugins/axios'
+
+export default {
+  name: "Example",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(47.41322, -1.219482),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+      showMap: true,
+    };
+  },
+  mounted() {
+    // get user location in browser and make call to api
+    navigator.geolocation.getCurrentPosition((pos) => {
+        var userLong = pos.coords.longitude;
+        var userLat = pos.coords.latitude;
+        console.log("User location:" + userLong + " " + userLat);
+        axios.get("/api/soup", {
+            data: {
+                longitude: userLong,
+                latitude: userLat
+            }
+        }).then((response) => {
+            console.log("Response:");
+            console.log(response);
+        });
+    });
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    },
+  },
+};
+</script>
