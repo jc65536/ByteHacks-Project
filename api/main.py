b8057877bea6cafc5a9ec6e1d124adf33bafd84b
Jason Cheng <jasoncheng224@gmail.com>
2020-08-01 17:05:00 -0700

    finished api for finding and getting details on soup kitchens

--- api/main.py
+++ api/main.py
@@ -3,18 +3,25 @@ from flask_login import current_user, login_required
 from .models import Job
 from .extensions import db
 import json
+import requests
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
+def get_data(data, property):
+    try:
+        return data[property]
+    except:
+        return None
 
 def row2dict(row):
     return dict((col, getattr(row, col)) for col in row.__table__.columns.keys())
 
-
 @main.route('/api/get-jobs', methods=["GET"])
 def get_jobs():
-    email = request.args.get("email")
-    employer = request.args.get('employer')
+    data = request.get_json()
+    email = get_data(data, "email")
+    employer = get_data(data, 'employer')
     if email is None and employer is None:
         all_jobs = Job.query.all()
     elif employer is not None:
@@ -23,7 +30,7 @@ def get_jobs():
         all_jobs = Job.query.filter_by(creator=email).all()
 
     wanted_jobs = [row2dict(job) for job in all_jobs]
-    sort_criteria = request.form.get("sort")
+    sort_criteria = get_data(data, "sort")
     if sort_criteria == "date" or sort_criteria is None:
         wanted_jobs = sorted(wanted_jobs, key=lambda job: job.date, reverse=True)
     # We might wanna implement this in the future
@@ -41,14 +48,15 @@ def get_jobs():
 @main.route('/api/add-job', methods=["POST"])
 @login_required
 def add_job():
-    title = request.form.get("title")
-    positions = request.form.get("positions")
-    location = request.form.get("location")
-    description = request.form.get("description")
-    start = request.form.get("start-date")
-    end = request.form.get("end-date")
-    wage = request.form.get("wage")
-    employer = request.form.get("employer")
+    data = request.get_json()
+    title = get_data(data, "title")
+    positions = get_data(data, "positions")
+    location = get_data(data, "location")
+    description = get_data(data, "description")
+    start = get_data(data, "start-date")
+    end = get_data(data, "end-date")
+    wage = get_data(data, "wage")
+    employer = get_data(data, "employer")
 
     # Will Assign The Job To Account Who Created It
     email = current_user.email
@@ -72,26 +80,74 @@ def show_page():
 @main.route("/api/update-job", methods=["POST"])
 @login_required
 def update_job():
-    job_id = request.form.get("id")
+    data = request.get_json()
+    job_id = get_data(data, "id")
     print(job_id)
     job = Job.query.filter_by(id=job_id, creator=current_user.email).first()
     print(job)
     if job:
-        setattr(job, 'title', request.form.get("title") or job.title)
-        setattr(job, 'positions', request.form.get("positions") or job.positions)
-        setattr(job, 'date', request.form.get("date") or job.date)
-        setattr(job, 'start-date', request.form.get("start-date") or job.start_date)
-        setattr(job, 'end-date', request.form.get("end-date") or job.end_date)
-        setattr(job, 'location', request.form.get("location") or job.location)
-        setattr(job, 'description', request.form.get("description") or job.description)
-        setattr(job, 'wage', request.form.get("wage") or job.wage)
-        setattr(job, 'employer', request.form.get("employer") or job.employer)
+        setattr(job, 'title', data["title"] or job.title)
+        setattr(job, 'positions', data["positions"] or job.positions)
+        setattr(job, 'date', data["date"] or job.date)
+        setattr(job, 'start-date', data["start-date"] or job.start_date)
+        setattr(job, 'end-date', data["end-date"] or job.end_date)
+        setattr(job, 'location', data["location"] or job.location)
+        setattr(job, 'description', data["description"] or job.description)
+        setattr(job, 'wage', data["wage"] or job.wage)
+        setattr(job, 'employer', data["employer"] or job.employer)
         db.session.commit()
     else:
         return "Not Authorized to Do This", 401
     return jsonify(
         id=job_id
     )
+
+# yelp categories searched: employmentagencies, foodbanks, homelessshelters
+@main.route("/api/soup", methods=["GET"])
+def get_soup():
+    data = request.get_json(force=True)
+    secret_file = open("yelpsecrets", "r")
+    yelp_id = secret_file.readline()
+    yelp_key = secret_file.readline().strip("\n")
+
+    place_id = get_data(data, "id")
+    if not(place_id is None):
+        details_response = requests.get("https://api.yelp.com/v3/businesses/" + place_id, headers={"Authorization": "Bearer " + yelp_key})
+        weekday = datetime.now().weekday()
+        try:
+            hours = details_response.json()["hours"][0]
+            if (hours["is_open_now"]):
+                return {"open": hours["open"][weekday]["start"], "close": hours["open"][weekday]["end"], "open_now": True, "next_open": weekday}
+            else:           # not open could be due to not open on that day or after closing hours
+                # search for the next open day
+                closed = True
+                while closed:
+                    weekday = (weekday + 1) % 7
+                    for day in hours["open"]:
+                        if day["day"] == weekday:
+                            closed = False
+                return {"open": hours["open"][weekday]["start"], "close": hours["open"][weekday]["end"], "open_now": False, "next_open": weekday}
+        except:         # no hours data
+            return "No hours data for this place."
+    
+    longitude = get_data(data, "longitude")
+    latitude = get_data(data, "latitude")
+    if ((longitude is None) or (latitude is None)):
+        return "Error: no location", 404
+    radius = (data["radius"] or 5) * 1600           # miles to meters
+
+    yelp_response = requests.get("https://api.yelp.com/v3/businesses/search", params={"longitude": longitude, "latitude": latitude, "radius": radius, "categories": "employmentagencies, foodbanks, homelessshelters"}, headers={"Authorization": "Bearer " + yelp_key})
+    businesses = yelp_response.json()["businesses"]
+
+    class SimplifiedBusiness:
+        def __init__(self, name, address, longitude, latitude, image):
+            self.name = name; self.address = address; self.longitude = longitude; self.latitude = latitude; self.image = image
+
+    simplified_array = []
+    for bus in businesses:
+        simplified_array.append(SimplifiedBusiness(bus["name"], "".join(bus["location"]["display_address"]), bus["coordinates"]["longitude"], bus["coordinates"]["latitude"], bus["image_url"]).__dict__)
+
+    return json.dumps(simplified_array)
 
 
 # Only Testing
